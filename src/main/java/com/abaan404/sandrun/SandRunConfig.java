e6e97e5dd4511b66427bb9b5e15c9dc8e9de325c
abaan404 <67100191+Abaan404@users.noreply.github.com>
2026-05-01 13:18:00 -0600

    fix: default config

--- src/main/java/com/abaan404/sandrun/SandRunConfig.java
+++ src/main/java/com/abaan404/sandrun/SandRunConfig.java
@@ -13,30 +13,37 @@ import xyz.nucleoid.plasmid.api.game.common.config.WaitingLobbyConfig;
 
 public record SandRunConfig(WaitingLobbyConfig players, Identifier map, boolean pvp, long maxDuration, int frequency,
         List<BlockState> blocks) {
+
+    public static final SandRunConfig DEFAULT = new SandRunConfig(
+            new WaitingLobbyConfig(2, 100),
+            Identifier.of("sandrun:example"),
+            false,
+            3 * 60 * 1000,
+            4,
+            List.of(
+                    Blocks.WHITE_CONCRETE_POWDER.getDefaultState(),
+                    Blocks.ORANGE_CONCRETE_POWDER.getDefaultState(),
+                    Blocks.MAGENTA_CONCRETE_POWDER.getDefaultState(),
+                    Blocks.LIGHT_BLUE_CONCRETE_POWDER.getDefaultState(),
+                    Blocks.YELLOW_CONCRETE_POWDER.getDefaultState(),
+                    Blocks.LIME_CONCRETE_POWDER.getDefaultState(),
+                    Blocks.PINK_CONCRETE_POWDER.getDefaultState(),
+                    Blocks.GRAY_CONCRETE_POWDER.getDefaultState(),
+                    Blocks.LIGHT_GRAY_CONCRETE_POWDER.getDefaultState(),
+                    Blocks.CYAN_CONCRETE_POWDER.getDefaultState(),
+                    Blocks.PURPLE_CONCRETE_POWDER.getDefaultState(),
+                    Blocks.BLUE_CONCRETE_POWDER.getDefaultState(),
+                    Blocks.BROWN_CONCRETE_POWDER.getDefaultState(),
+                    Blocks.GREEN_CONCRETE_POWDER.getDefaultState(),
+                    Blocks.RED_CONCRETE_POWDER.getDefaultState(),
+                    Blocks.BLACK_CONCRETE_POWDER.getDefaultState()));
+
     public static final MapCodec<SandRunConfig> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
-            WaitingLobbyConfig.CODEC.fieldOf("players").forGetter(SandRunConfig::players),
-            Identifier.CODEC.fieldOf("map").forGetter(SandRunConfig::map),
-            Codec.BOOL.optionalFieldOf("pvp", false).forGetter(SandRunConfig::pvp),
-            Codec.LONG.optionalFieldOf("max_duration", (long) (3 * 60 * 1000)).forGetter(SandRunConfig::maxDuration),
-            Codec.INT.optionalFieldOf("frequency", 4).forGetter(SandRunConfig::frequency),
-            Codec.list(BlockState.CODEC)
-                    .optionalFieldOf("blocks", List.of(
-                            Blocks.WHITE_CONCRETE_POWDER.getDefaultState(),
-                            Blocks.ORANGE_CONCRETE_POWDER.getDefaultState(),
-                            Blocks.MAGENTA_CONCRETE_POWDER.getDefaultState(),
-                            Blocks.LIGHT_BLUE_CONCRETE_POWDER.getDefaultState(),
-                            Blocks.YELLOW_CONCRETE_POWDER.getDefaultState(),
-                            Blocks.LIME_CONCRETE_POWDER.getDefaultState(),
-                            Blocks.PINK_CONCRETE_POWDER.getDefaultState(),
-                            Blocks.GRAY_CONCRETE_POWDER.getDefaultState(),
-                            Blocks.LIGHT_GRAY_CONCRETE_POWDER.getDefaultState(),
-                            Blocks.CYAN_CONCRETE_POWDER.getDefaultState(),
-                            Blocks.PURPLE_CONCRETE_POWDER.getDefaultState(),
-                            Blocks.BLUE_CONCRETE_POWDER.getDefaultState(),
-                            Blocks.BROWN_CONCRETE_POWDER.getDefaultState(),
-                            Blocks.GREEN_CONCRETE_POWDER.getDefaultState(),
-                            Blocks.RED_CONCRETE_POWDER.getDefaultState(),
-                            Blocks.BLACK_CONCRETE_POWDER.getDefaultState()))
-                    .forGetter(SandRunConfig::blocks))
+            WaitingLobbyConfig.CODEC.optionalFieldOf("players", DEFAULT.players).forGetter(SandRunConfig::players),
+            Identifier.CODEC.optionalFieldOf("map", DEFAULT.map).forGetter(SandRunConfig::map),
+            Codec.BOOL.optionalFieldOf("pvp", DEFAULT.pvp).forGetter(SandRunConfig::pvp),
+            Codec.LONG.optionalFieldOf("max_duration", DEFAULT.maxDuration).forGetter(SandRunConfig::maxDuration),
+            Codec.INT.optionalFieldOf("frequency", DEFAULT.frequency).forGetter(SandRunConfig::frequency),
+            Codec.list(BlockState.CODEC).optionalFieldOf("blocks", DEFAULT.blocks).forGetter(SandRunConfig::blocks))
             .apply(instance, SandRunConfig::new));
 }
